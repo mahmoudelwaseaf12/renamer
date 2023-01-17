2b34d95490ba1b15f2f4a0ba659a3683d1fb6229
mahmoudelwaseaf12 <121382025+mahmoudelwaseaf12@users.noreply.github.com>
2023-01-17 02:57:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ API_ID = os.environ.get("API_ID", "22745599")
 
 API_HASH = os.environ.get("API_HASH", "3f0b55d67e0357d426afc5dc3c27145c")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5681484220:AAE6i-pCvDqjNMhzDfVlPcP6D_fLQXHOzw4") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5855550294:AAGUumch418LHmU0luyjVH1a3rI_MlmxfWo") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "-1001860403687") 
 
-DB_NAME = os.environ.get("DB_NAME","MahmoudElwaseaf")     
+DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","postgres://mahmoudelwaseaf_user:YLKFV4uboH6RT0NikB00v6mLWk0mBXga@dpg-cf2uohp4reb5o46uujvg-a.frankfurt-postgres.render.com/mahmoudelwaseaf")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://hmoelwaseaf:ghZvWN9xDWkbjizg@cluster0.gqmhx8j.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
